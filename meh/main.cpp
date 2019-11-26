a89b0589b2d476faead8ad00863e438453354b8e
headwedge <headwedge@hotmail.co.uk>
2019-11-26 21:45:57 +0000

    tokens

--- meh/main.cpp
+++ meh/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 
 int main() {
 
 	std::cout << "meh\n";
+
+	while (true) {
+
+		std::string s, t;
+
+		while (!s.length()) {
+			getline(std::cin, s);
+		}
+
+		if (s == "q") {
+			break;
+		}
+
+		std::stringstream ss(s);
+
+		while (ss >> t) {
+			std::cout << t << "\n";
+		}
+
+	}
 
 }
