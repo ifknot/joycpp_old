b3e0415f3c3cd652ead61bc584f58208779f65c1
headwedge <headwedge@hotmail.co.uk>
2019-11-27 22:34:20 +0000

    colours

--- meh/main.cpp
+++ meh/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <map>
+#include <stack>
+#include <functional>
+#include <cstdlib>
+#include <stdexcept>
+
+using stack_t = std::stack<double>;
+using dictionary_t = std::map<std::string, std::function<void()>>;
+
+stack_t s;
+
+bool check(size_t n) {
+	if (s.size() < n) {
+		std::cout << "stack underflow";
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+dictionary_t d = {
+	{".",		[&]() { if (check(1)) { std::cout << "\033[31m" << s.top(); s.pop(); } }},
+	{".s",		[&]() {}},
+	{"+",		[&]() { if (check(2)) { auto x = s.top(); s.pop(); s.top() += x; } }},
+	{"bye",		[&]() { std::exit(0); }}
+
+};
 
 int main() {
 
@@ -8,20 +36,31 @@ int main() {
 
 	while (true) {
 
-		std::string s, t;
+		std::string line, t;
 
-		while (!s.length()) {
-			getline(std::cin, s);
+		while (!line.length()) {
+			getline(std::cin, line);
 		}
 
-		if (s == "q") {
-			break;
-		}
-
-		std::stringstream ss(s);
+		std::stringstream ss(line);
 
 		while (ss >> t) {
-			std::cout << t << "\n";
+			auto it = d.find(t);
+			if (it != d.end()) {
+				(it->second)();
+			} 
+			else {
+				try {
+					double x = std::stod(t);
+					s.push(x);
+				}
+				catch (std::invalid_argument) {
+					std::cout << "no conversion could be performed\n";
+				}
+				catch (std::out_of_range) {
+					std::cout << "converted value would fall out of the range of the result type\n";
+				}
+			}
 		}
 
 	}
