70add7ea8aa19e081816dc6f26366afb46846fbc
headwedge <headwedge@hotmail.co.uk>
2019-11-28 00:33:45 +0000

    somewhat functional

--- meh/main.cpp
+++ meh/main.cpp
@@ -2,37 +2,65 @@
 #include <string>
 #include <sstream>
 #include <map>
-#include <stack>
+#include <vector>
 #include <functional>
 #include <cstdlib>
 #include <stdexcept>
 
-using stack_t = std::stack<double>;
-using dictionary_t = std::map<std::string, std::function<void()>>;
+#include "colour_codes.h"
+
+using namespace std;
+
+using stack_t = vector<string>;
+using dictionary_t = map<string, function<void()>>;
 
 stack_t s;
 
-bool check(size_t n) {
+bool arg(size_t n) {
 	if (s.size() < n) {
-		std::cout << "stack underflow";
+		cout << RED << "stack underflow";
 		return false;
 	}
 	else {
-		return true;
+		return true; 
 	}
 }
 
+bool flag{ true };
+
 dictionary_t d = {
-	{".",		[&]() { if (check(1)) { std::cout << "\033[31m" << s.top(); s.pop(); } }},
-	{".s",		[&]() {}},
-	{"+",		[&]() { if (check(2)) { auto x = s.top(); s.pop(); s.top() += x; } }},
+	{".",		[&]() { if (arg(1)) { cout << BOLDWHITE << s.back(); } }},
+	{".s",		[&]() { for (auto& i : s) { cout << i << " "; } }},
+	//quotations
+	{"[",		[&]() { flag = false; s.push_back("["); }},
+	{"]",		[&]() { flag = true; }},
+	//combinators
+	{"i",		[&]() { //read from [ -- ] constructing a string then exec the string}},
+	//stack operations
+	{"dup",		[&]() { if (arg(1)) { s.push_back(s.back()); } }},
+	{"pop",		[&]() { if (arg(1)) { s.pop_back(); } }},
+	{"swap",	[&]() { if (arg(2)) { auto x = s[s.size() - 1]; s[s.size() - 1] = s[s.size() - 2]; s[s.size() - 2] = x; } }},
+	//math
+	{"+",		[&]() { if (arg(2)) { double x, y; //if(num(2)) also if(list(2))
+						x = stod(s.back()); 
+						s.pop_back(); 
+						y = stod(s.back()); 
+						s.pop_back();
+						s.push_back(to_string(x + y)); } }},
+	{"*",		[&]() { if (arg(2)) { double x, y; 
+						x = stod(s.back()); 
+						s.pop_back(); 
+						y = stod(s.back()); 
+						s.pop_back();
+						s.push_back(to_string(x * y)); } }},
+	//special
 	{"bye",		[&]() { std::exit(0); }}
 
 };
 
 int main() {
 
-	std::cout << "meh\n";
+	std::cout << WHITE << "meh\n";
 
 	while (true) {
 
@@ -47,21 +75,31 @@ int main() {
 		while (ss >> t) {
 			auto it = d.find(t);
 			if (it != d.end()) {
-				(it->second)();
+				if (flag) {
+					(it->second)();
+				}
+				else if (t == "]") {
+					flag = true;
+					s.push_back(t);
+				}
+				else {
+					s.push_back(it->first);
+				}
 			} 
 			else {
 				try {
 					double x = std::stod(t);
-					s.push(x);
+					s.push_back(std::to_string(x));
 				}
 				catch (std::invalid_argument) {
-					std::cout << "no conversion could be performed\n";
+					std::cout << RED << "no conversion";
 				}
 				catch (std::out_of_range) {
-					std::cout << "converted value would fall out of the range of the result type\n";
+					std::cout << RED << "\aconverted value would fall out of the range of the result type";
 				}
 			}
 		}
+		std::cout << BOLDBLACK << "\tok\n" << RESET;
 
 	}
 
